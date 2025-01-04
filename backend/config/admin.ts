e7cbff1c00e41db9623bb3a53f6a9023569b91fb
WESGAGU <weslin2016garcia@gmail.com>
2025-01-03 18:08:01 -0600

    update

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,17 +1,25 @@
-export default ({ env }) => ({
+// Define el tipo para la función env
+type EnvFunction = {
+  (key: string): string | undefined; // Para valores de tipo string
+  bool(key: string, defaultValue?: boolean): boolean; // Para valores de tipo boolean
+  int(key: string, defaultValue?: number): number; // Para valores de tipo number
+  array(key: string, defaultValue?: string[]): string[]; // Para valores de tipo array
+};
+
+export default ({ env }: { env: EnvFunction }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET'), // Usa la variable de entorno ADMIN_JWT_SECRET
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT'), // Usa la variable de entorno API_TOKEN_SALT
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT'), // Usa la variable de entorno TRANSFER_TOKEN_SALT
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool('FLAG_NPS', true), // Usa la variable de entorno FLAG_NPS (con valor predeterminado true)
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true), // Usa la variable de entorno FLAG_PROMOTE_EE (con valor predeterminado true)
   },
 });
